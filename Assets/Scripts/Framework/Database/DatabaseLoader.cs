ab29d2d0fc6ec968f6f1e6d898e095821cb25e9c
zhuang jr <1095313400@qq.com>
2023-07-05 18:34:24 +0800

    feature: 配置运行时热更新功能

--- Assets/Scripts/Framework/Database/DatabaseLoader.cs
+++ Assets/Scripts/Framework/Database/DatabaseLoader.cs
@@ -7,8 +7,13 @@ using UnityEngine;
 public static class DatabaseLoader
 {
     private static readonly MethodInfo methodAddToDatabase = typeof(DatabaseLoader).GetMethod("AddToDatabseGeneric", BindingFlags.NonPublic | BindingFlags.Static);
+    private static readonly MethodInfo methodHotUpdateConfig = typeof(DatabaseLoader).GetMethod("HotUpdateConfigGeneric", BindingFlags.NonPublic | BindingFlags.Static);
     private static readonly Dictionary<Type, MethodInfo> methodAddToDatabaseTyped = new Dictionary<Type, MethodInfo>();
     private static int count = 0;
+
+    /// <summary>
+    /// 加载所有配置
+    /// </summary>
     public static void Load()
     {
         Dictionary<Type, List<BaseConfig>> content = new Dictionary<Type, List<BaseConfig>>();
@@ -35,7 +40,7 @@ public static class DatabaseLoader
             {
                 try
                 {
-                    AddToDatabse(pair.Key, config);
+                    AddToDatabse(config);
                 }
                 catch (Exception e)
                 {
@@ -45,6 +50,43 @@ public static class DatabaseLoader
         }
 
         Debug.Log(TextColor.Green(string.Format("加载了 {0} 个配置", count)));
+    }
+
+    /// <summary>
+    /// 热更新所有配置
+    /// </summary>
+    public static void HotUpdate()
+    {
+        Dictionary<Type, List<BaseConfig>> content = new Dictionary<Type, List<BaseConfig>>();
+        XmlConfigLoader loader = new XmlConfigLoader();
+
+        loader.Load();
+
+        foreach (BaseConfig config in loader.Content)
+        {
+            Type type = config.GetType();
+            if (!content.TryGetValue(type, out List<BaseConfig> list))
+            {
+                list = new List<BaseConfig>();
+                content.Add(type, list);
+            }
+            list.Add(config);
+        }
+
+        foreach (var pair in content)
+        {
+            foreach (BaseConfig config in pair.Value)
+            {
+                try
+                {
+                    HotUpdateConfig(config);
+                }
+                catch (Exception e)
+                {
+                    Debug.LogError(e);
+                }
+            }
+        }
     }
 
     private static void AddToDatabse(BaseConfig config)
@@ -67,6 +109,34 @@ public static class DatabaseLoader
         try
         {
             Database<T>.Add(config);
+            count++;
+        }
+        catch (Exception e)
+        {
+            Debug.LogError(e.ToString());
+        }
+    }
+
+    private static void HotUpdateConfig(BaseConfig config)
+    {
+        HotUpdateConfig(config.GetType(), config);
+    }
+
+    private static void HotUpdateConfig(Type type, BaseConfig config)
+    {
+        if (!methodAddToDatabaseTyped.TryGetValue(type, out MethodInfo method))
+        {
+            method = methodHotUpdateConfig.MakeGenericMethod(type);
+            methodAddToDatabaseTyped.Add(type, method);
+        }
+        method.Invoke(null, new object[] { config });
+    }
+
+    private static void HotUpdateConfigGeneric<T>(T config) where T : BaseConfig
+    {
+        try
+        {
+            Database<T>.HotUpdate(config);
             count++;
         }
         catch (Exception e)
