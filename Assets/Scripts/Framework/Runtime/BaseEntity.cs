2e5c6b0e066effbd1cf41f0ee1ee80d82397394c
zhuang jr <1095313400@qq.com>
2023-07-06 14:57:41 +0800

    feature: 实体接收消息

--- Assets/Scripts/Framework/Runtime/BaseEntity.cs
+++ Assets/Scripts/Framework/Runtime/BaseEntity.cs
@@ -1,8 +1,11 @@
 using System;
 
-public class BaseEntity<TConfig> : IEntity where TConfig : BaseConfig
+using Vocore;
+
+public class BaseEntity<TConfig> : IEntity, IEventReciever where TConfig : BaseConfig
 {
     private TConfig _config;
+    private bool _isDestroyed = false;
     public TConfig Config
     {
         get
@@ -14,12 +17,34 @@ public class BaseEntity<TConfig> : IEntity where TConfig : BaseConfig
     public void Initialize(TConfig config)
     {
         _config = config;
+        _isDestroyed = false;
         OnCreate();
+        OnBindingEvent();
     }
 
     public void Destroy()
     {
+        _isDestroyed = true;
+        OnBindingEvent();
         OnDestroy();
+        _config = null;
+    }
+
+    protected void BindEvent<TData>(EventId eventId, Action<TData> callback)
+    {
+        if (_isDestroyed)
+        {
+            this.UnregisterEvent(eventId);
+        }
+        else
+        {
+            this.RegisterEvent(eventId, callback);
+        }
+    }
+
+    protected virtual void OnBindingEvent()
+    {
+        
     }
 
     public virtual void OnCreate()
--- Assets/Scripts/Framework/Runtime/BaseThing.cs
+++ Assets/Scripts/Framework/Runtime/BaseThing.cs
@@ -1,10 +1,12 @@
 using System;
 
 using UnityEngine;
+using Vocore;
 
-public class BaseThing<TConfig> : IEntity where TConfig : BaseConfig
+public class BaseThing<TConfig> : IEntity, IEventReciever where TConfig : BaseConfig
 {
     private TConfig _config;
+    private bool _isDestroyed = false;
     private GameObject _instance;
     public TConfig Config
     {
@@ -26,13 +28,35 @@ public class BaseThing<TConfig> : IEntity where TConfig : BaseConfig
     {
         _config = config;
         _instance = instance;
+        _isDestroyed = false;
         OnCreate();
+        OnBindingEvent();
     }
 
     public void Destroy()
     {
+        _isDestroyed = true;
+        OnBindingEvent();
         OnDestroy();
         GameObject.Destroy(_instance);
+        _config = null;
+    }
+
+    protected void BindEvent<TData>(EventId eventId, Action<TData> callback)
+    {
+        if (_isDestroyed)
+        {
+            this.UnregisterEvent(eventId);
+        }
+        else
+        {
+            this.RegisterEvent(eventId, callback);
+        }
+    }
+
+    protected virtual void OnBindingEvent()
+    {
+
     }
 
     public virtual void OnCreate()
