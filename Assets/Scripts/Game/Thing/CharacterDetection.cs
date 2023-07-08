5ad03ccad2f7e20bea1e59d63120295387b06c18
JaMesLiMers <jameslimers@gmail.com>
2023-07-08 20:33:10 +0800

    feature: add charactor object detection script

---
+++ Assets/Scripts/Game/Thing/CharacterDetection.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterDetection : BaseThing<CharacterDetectionConfig>
+{
+    public float detectionRange; 
+    public override void OnUpdate()
+    {
+    }
+}
