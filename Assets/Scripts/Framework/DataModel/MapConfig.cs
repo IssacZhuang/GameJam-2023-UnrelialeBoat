dd27ad49a2f4d61391153ed7ccd72821e329e635
zhuang jr <1095313400@qq.com>
2023-07-09 02:33:44 +0800

    opt: map config注释

--- Assets/Scripts/Framework/DataModel/MapConfig.cs
+++ Assets/Scripts/Framework/DataModel/MapConfig.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MapConfig: BaseConfig
 {
-    public string scene;
+    public string scene;//地图prefab的addressable id
     public string description;
     public List<Type> components;
 }
