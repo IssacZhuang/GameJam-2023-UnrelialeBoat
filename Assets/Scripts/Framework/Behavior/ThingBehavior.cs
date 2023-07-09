429b761942fbe908198c587f8bda30538daad3e2
zhuang jr <1095313400@qq.com>
2023-07-09 18:37:52 +0800

    feature: behavior暴露Entity

--- Assets/Scripts/Framework/Behavior/ThingBehavior.cs
+++ Assets/Scripts/Framework/Behavior/ThingBehavior.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 public class ThingBehavior<TThing, TConfig> : MonoBehaviour where TThing: BaseThing<TConfig> where TConfig: BaseThingConfig
 {
     public string configName;
+    public TThing Entity{get; private set;}
     void Start()
     {
         TThing thing = Activator.CreateInstance<TThing>();
         thing.Initialize(Content.GetConfig<TConfig>(configName), this.gameObject);
+        Entity = thing;
         Current.Map.AddEntity(thing);
     }
+
+
 }
